bdfda35a79838af64749af226995ba020c8664aa
joellokota@gmail.com <joellokota@gmail.com>
2025-02-15 17:48:01 +0100

    feat: my first commit

---
+++ ClientConseiller/src/main/java/com/bpce/clientconseiller/ClientConseillerApplication.java
@@ -0,0 +1,13 @@
+package com.bpce.clientconseiller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientConseillerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientConseillerApplication.class, args);
+	}
+
+}
---
+++ ClientConseiller/src/test/java/com/bpce/clientconseiller/ClientConseillerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpce.clientconseiller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientConseillerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
